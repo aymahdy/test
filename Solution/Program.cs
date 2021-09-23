edca1d6d74373ff1339045e5d6ef629eb7a259d8
Ahmed Youssef <amahdy.c@channels.com.sa>
2021-09-23 15:39:02 +0200

    Initial Commit

---
+++ Solution/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            ILogger<string> logger = new Logger<string>();
+            IDataStore dataStore = new DataStore(logger);
+            string data;
+            logger.Write("Program: Program Started");
+            dataStore.Create("testkey", "testdata");
+            dataStore.TryRead("testkey2", out data);
+            dataStore.TryRead("testkey1", out data);
+            dataStore.Update("testkey", "testdata2");
+            dataStore.Delete("testkey");
+            logger.Write("Program: Program Finished");
+
+        }
+    }
+
+
+    public interface IDataStore
+    {
+        bool Create(string key, string data);
+        bool TryRead(string key, out string data);
+        bool Update(string key, string data);
+        bool Delete(string key);
+    }
+
+    public class DataStore : IDataStore
+    {
+        private readonly string _baseMessage;
+        private Dictionary<string, string> _keyDataPairs;
+        private readonly ILogger<string> _logger;
+        public DataStore(ILogger<string> logger)
+        {
+            _logger = logger;
+            _keyDataPairs = new Dictionary<string, string>();
+            _baseMessage = "MyDataStore: ";
+        }
+        public bool Create(string key, string data)
+        {
+            try
+            {
+                _logger.Write($"{_baseMessage}Call - Create ('{key}','{data}')");
+                _keyDataPairs.Add(key, data);
+                _logger.Write($"{_baseMessage}Result - Entry with key '{key}' and data '{data}' created");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public bool TryRead(string key, out string data)
+        {
+            try
+            {
+                _logger.Write($"{_baseMessage}Call - Read ('{key}')");
+                data = _keyDataPairs.FirstOrDefault(a => a.Key == key).Value;
+                _logger.Write(string.IsNullOrWhiteSpace(data)
+                    ? $"{_baseMessage}Result - Entry with key '{key}' does not exists"
+                    : $"{_baseMessage}Result - Entry with key '{key}' returned");
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                data = null;
+                return false;
+            }
+        }
+
+        public bool Update(string key, string data)
+        {
+            try
+            {
+                _logger.Write($"{_baseMessage}Call - Update ('{key}','{data}')");
+                _keyDataPairs[key] = data;
+                _logger.Write($"{_baseMessage}Result - Entry with key '{key}' updated with the data '{data}'");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public bool Delete(string key)
+        {
+            try
+            {
+                _logger.Write($"{_baseMessage}Call - Remove ('{key}')");
+                _keyDataPairs.Remove(key);
+                _logger.Write($"{_baseMessage}Result - Entry with key '{key}' removed");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+    public interface ILogger<T>
+    {
+        void Write(T message);
+    }
+
+    public class Logger<T> : ILogger<T>
+    {
+        public void Write(T message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SolutionUnitTest/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Castle.Core.Internal;
+using Solution;
+using Xunit;
+using NSubstitute;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace SolutionUnitTest
+{
+    public class SolutionTests
+    {
+
+        private readonly IDataStore dataStore;
+        private readonly ILogger<string> _logger;
+        public SolutionTests()
+        {
+            _logger = Substitute.For<ILogger<string>>();
+            dataStore = new DataStore(_logger);
+        }
+        [Fact]
+        public void WhenCreate()
+        {
+            //Act
+            var result = dataStore.Create("Key1", "Data1");
+            //Assert
+            result.Equals(true);
+        }
+        [Fact]
+        public void WhenRead_ReturnsNull()
+        {
+            //Arrange
+            string outResult;
+            dataStore.Create("Key1", "data1");
+
+            //Act
+            var result = dataStore.TryRead("Key2", out outResult);
+
+            //Assert
+            outResult.IsNullOrEmpty();
+        }
+
+
+        [Fact]
+        public void WhenRead_ReturnsNotNull()
+        {
+            //Arrange
+            string outResult;
+            dataStore.Create("Key1", "data1");
+
+            //Act
+            var result = dataStore.TryRead("Key1", out outResult);
+
+            //Assert
+            Assert.IsNotNull(outResult);
+        }
+
+        [Fact]
+        public void WhenUpdate()
+        {
+            //Arrange
+            dataStore.Create("Key1", "data1");
+
+            //Act
+            var result = dataStore.Update("Key1", "data2");
+
+            //Assert
+            result.Equals(true);
+        }
+
+        [Fact]
+        public void WhenRemove_ReturnTrue()
+        {
+            //Arrange
+            dataStore.Create("Key1", "data1");
+
+            //Act
+            var result = dataStore.Delete("Key1");
+
+            //Assert
+            result.Equals(true);
+        }
+    }
+}
